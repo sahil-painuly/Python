143bf77c29bcc30df11f04ebaf1626eeeebc28b7
Sahil Painuly <166888935+sahil-painuly@users.noreply.github.com>
2024-04-15 22:46:35 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,11 @@
+tuple = (1,2,3) #immutable 
+# tuple[0] = 0 not allowed
+
+# empty tuble 
+tup = ()
+
+# tuble functions
+
+print(tup.index(2)) #returns index of first occurrence
+print(tup.count(3)) #counts total occurrences tup.count(1) is 2
+
